c7afe5e69be69e5bec3384cb4c0ef4ffbd5c6742
Oleg Kl <oklyavlin@tourmalinecore.com>
2022-12-01 15:29:04 +0500

    build: #37bq68m: add build config for deploy

--- UserManagementService.Api/Program.cs
+++ UserManagementService.Api/Program.cs
@@ -1,5 +1,7 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.EventLog;
 using UserManagementService.Application;
 using UserManagementService.DataAccess;
 
@@ -13,7 +15,65 @@ var environment = builder.Environment;
 builder.Services.AddControllers();
 
 builder.Services.AddApplication();
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    var env = hostingContext.HostingEnvironment;
+
+    var reloadOnChange = hostingContext.Configuration.GetValue("hostBuilder:reloadConfigOnChange", true);
+
+    config.AddJsonFile("appsettings.json", true, reloadOnChange)
+        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, reloadOnChange)
+        .AddJsonFile("appsettings.Active.json", true, reloadOnChange);
+
+    if (env.IsDevelopment() && !string.IsNullOrEmpty(env.ApplicationName))
+    {
+        var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
+
+        config.AddUserSecrets(appAssembly, true);
+    }
+
+    config.AddEnvironmentVariables();
+
+    if (args != null)
+    {
+        config.AddCommandLine(args);
+    }
+});
+
 builder.Services.AddPersistence(configuration);
+
+builder.Host.ConfigureLogging((hostingContext, logging) =>
+{
+    var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+    // IMPORTANT: This needs to be added *before* configuration is loaded, this lets
+    // the defaults be overridden by the configuration.
+    if (isWindows)
+    {
+        // Default the EventLogLoggerProvider to warning or above
+        logging.AddFilter<EventLogLoggerProvider>(level => level >= LogLevel.Warning);
+    }
+
+    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    logging.AddConsole();
+    logging.AddDebug();
+    logging.AddEventSourceLogger();
+
+    if (isWindows)
+    {
+        // Add the EventLogLoggerProvider on windows machines
+        logging.AddEventLog();
+    }
+
+    logging.Configure(options =>
+    {
+        options.ActivityTrackingOptions = ActivityTrackingOptions.SpanId
+                                          | ActivityTrackingOptions.TraceId
+                                          | ActivityTrackingOptions.ParentId;
+    }
+        );
+});
 
 var app = builder.Build();
 
