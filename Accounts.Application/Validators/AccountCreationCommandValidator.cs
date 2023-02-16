c7d3b36cd6cbf497fa4b1c98c0b76f1e3b431401
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-02-16 12:19:39 +0500

    fix: add validation to validations for duplicates of incoming roles

--- Accounts.Application/Validators/AccountCreationCommandValidator.cs
+++ Accounts.Application/Validators/AccountCreationCommandValidator.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using Accounts.Application.Users.Commands;
 using Accounts.Core.Contracts;
@@ -24,6 +25,7 @@ namespace Accounts.Application.Validators
             RuleFor(x => x.RoleIds)
                 .NotNull()
                 .NotEmpty()
+                .Must(CheckDublicate)
                 .MustAsync(
                     async (accountRoleIds, _) =>
                     {
@@ -33,6 +35,13 @@ namespace Accounts.Application.Validators
                         return accountRoleIds.All(x => roleIds.Contains(x));
                     })
                 .WithMessage("Incorrect role ids. Probably you tried to set unavailable role id");
+        }
+        private bool CheckDublicate(List<long> roleIds)
+        {
+            return roleIds.GroupBy(x => x)
+                 .Where(y => y.Count() > 1)
+                 .Select(z => z.Key)
+                 .ToList().Count == 0;
         }
     }
 }
