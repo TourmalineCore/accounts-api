3e98153192593e6476939fe4ea3954c7a32c5570
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2025-10-29 15:42:11 +0500

    feat: add permission for delete book copies

--- Core/Models/Permissions.cs
+++ Core/Models/Permissions.cs
@@ -30,6 +30,8 @@ public static class Permissions
   public const string CanManageBooks = "CanManageBooks";
   public const string IsBooksHardDeleteAllowed = "IsBooksHardDeleteAllowed";
 
+  public const string IsBookCopiesHardDeleteAllowed = "IsBookCopiesHardDeleteAllowed";
+
   public static bool IsPermissionExists(string permissionName)
   {
     var permissionNames = GetPermissionNames();
